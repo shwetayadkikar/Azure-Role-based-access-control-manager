b9c6a393bf238b8957cb8e67765cdd18af5ecbcd
Shweta Yadkikar <shweta.yadkikar@zevenseas.com>
2016-11-22 15:25:07 +0530

    graph api helper

---
+++ RBACManager/Services/AzureGraphAPIHelper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure.ActiveDirectory.GraphClient;
+using Microsoft.Azure.ActiveDirectory.GraphClient.Extensions;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace RBACManager.Services
+{
+    public class AzureGraphAPIHelper
+    {
+        private static string GraphResourceId = ConfigurationManager.AppSettings["GraphUrl"];
+        private static string TenantId = ConfigurationManager.AppSettings["TenantId"];
+
+        public static string AccessToken
+        {
+            get; set;
+        }
+
+        public static async Task<string> AcquireTokenAsync()
+        {
+            if (AccessToken == null || string.IsNullOrEmpty(AccessToken))
+            {
+                var authContext = await AzureAuthenticationManager.GetAccessTokenAsync(GraphResourceId);
+                AccessToken = authContext.AccessToken;
+            }
+            return AccessToken;
+        }
+
+        public static ActiveDirectoryClient GetActiveDirectoryClient()
+        {
+            Uri baseServiceUri = new Uri(GraphResourceId);
+            ActiveDirectoryClient activeDirectoryClient =
+                new ActiveDirectoryClient(new Uri(baseServiceUri, TenantId), async () => await AcquireTokenAsync());
+            return activeDirectoryClient;
+        }
+    }
+}
